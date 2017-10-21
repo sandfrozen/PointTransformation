2d296e5ad08d371f05eca07cfb78c7c1162545ef
Tomek Buslowski <tbuslowski@gmail.com>
2017-10-21 10:35:27 +0200

    Initial Commit

---
+++ ImageCocoaApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageCocoaApp
+//
+//  Created by Tomek Buslowski on 21.10.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImageCocoaApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageCocoaApp
+//
+//  Created by Tomek Buslowski on 21.10.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
