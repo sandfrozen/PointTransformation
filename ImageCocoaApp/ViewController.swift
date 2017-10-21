98ee7d55af945e33edaf8cf33bb8663f085839e6
Tomek Buslowski <tbuslowski@gmail.com>
2017-10-21 23:49:34 +0200

    Image Loading, bitmap, prepared UI

--- ImageCocoaApp/ViewController.swift
+++ ImageCocoaApp/ViewController.swift
@@ -7,21 +7,49 @@
 //
 
 import Cocoa
+import Quartz
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var imagePathLabel: NSTextField!
+    @IBOutlet weak var imageView: IKImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
+    
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    @IBAction func openImageAction(_ sender: Any) {
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose a image";
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseDirectories    = true;
+        dialog.canCreateDirectories    = true;
+        dialog.allowsMultipleSelection = false;
+        dialog.allowedFileTypes        = ["png", "jpg"];
+        
+        if dialog.runModal() == NSApplication.ModalResponse.OK {
+            if let result = dialog.url {
+                self.imagePathLabel.stringValue = result.lastPathComponent
+                self.imageView.setImageWith(result)
+                
+                let nsData = NSData(contentsOf: result)
+                let bitMap = NSBitmapImageRep(data: nsData! as Data)
+                print(bitMap?.size.width)
+                print(bitMap?.size.height)
+                print(bitMap?.setColor(NSColor.blue, atX: 1, y: 1))
+                print(bitMap?.colorAt(x: 1, y: 1))
+
+                
+            }
+        } else {
+            return
         }
     }
-
-
+    
+    
 }
 
